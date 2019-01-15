efd63be2537d5a109c074c2a441ff71b3d283b7c
dchanthavong <delphine.chanthavong@isep.fr>
2019-01-15 21:33:03 +0100

    " ============================================================================ " Netrw Directory Listing                                        (netrw v140) "   /Users/delphinechanthavong/Documents/project/RentalStore/sakila-business-webapi/src/main/resources/static/js "   Sorted by      name "   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$ "   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:exec " ============================================================================ ../ controller/ service/ app.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,11 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { VehicleComponent } from './vehicle/vehicle.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
@@ -34,7 +39,9 @@ import { HeaderComponent } from './header/header.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule,
+    MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatInputModule,
+    MatExpansionModule, MatGridListModule,
+    FormsModule, ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicles',
@@ -10,12 +11,35 @@ import { Observable } from 'rxjs';
 export class VehiclesComponent implements OnInit {
 
   title = 'vehicles page';
-  items: Observable<any[]>;
-  constructor(db: AngularFirestore) {
-    this.items = db.collection('brand').valueChanges();
+  brands: Observable<any[]>;
+  vehicles: Observable<any[]>;
+
+  myForm: FormGroup;
+
+  constructor(db: AngularFirestore, private fb: FormBuilder) {
+    this.brands = db.collection('brand').valueChanges();
+    this.vehicles = db.collection('vehicle').valueChanges();
   }
 
   ngOnInit() {
+    this.myForm = this.fb.group({
+      brand: ['', [Validators.maxLength(20)]],
+      gearbox: [''],
+      minPrice: [''],
+      maxPrice: ['']
+    });
+  }
+
+  clearFilters() {
+    this.myForm.setValue({
+      brand: '',
+      gearbox: '',
+      minPrice: '',
+      maxPrice: ''
+    });
+  }
+
+  applyFilters() {
   }
 
 }
